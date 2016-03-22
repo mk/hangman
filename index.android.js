4c7bc881e923983f002412bafcacf9d3845b6f03
Martin Kavalar <mk@katercalling.com>
2016-03-22 19:13:55 +0100

    Convert to elm-native-ui project

--- index.android.js
+++ index.android.js
@@ -1,51 +1,10 @@
 /**
- * Sample React Native App
+ * Sample Elm Native UI app
+ * https://github.com/elm-native-ui/elm-native-ui
  * https://github.com/facebook/react-native
  */
+'use strict';
+var {AppRegistry} = require('react-native');
+var ElmAppWrapper = require('./ElmAppWrapper');
 
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-class Hangman extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('Hangman', () => Hangman);
+AppRegistry.registerComponent('Hangman', ElmAppWrapper);
